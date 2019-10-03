3343b6ec0fc65c42703cb9cc16a2363db12c38c0
Andreas Holstenson <a@holstenson.se>
2019-10-03 17:56:23 +0200

    feat: Adding DayOfWeek

---
+++ src/DayOfWeek.ts
@@ -0,0 +1,39 @@
+/**
+ * Enum representing the day of week.
+ */
+export enum DayOfWeek {
+	/**
+	 * Sunday.
+	 */
+	Sunday = 0,
+
+	/**
+	 * Monday.
+	 */
+	Monday = 1,
+
+	/**
+	 * Tuesday.
+	 */
+	Tuesday = 2,
+
+	/**
+	 * Wednesday.
+	 */
+	Wednesday = 3,
+
+	/**
+	 * Thursday.
+	 */
+	Thursday = 4,
+
+	/**
+	 * Friday.
+	 */
+	Friday = 5,
+
+	/**
+	 * Saturday.
+	 */
+	Saturday = 6
+}
