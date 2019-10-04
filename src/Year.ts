1ddf6c125cc1921831eb08b66764b854eb78c87e
Andreas Holstenson <a@holstenson.se>
2019-10-04 18:26:35 +0200

    feat: from and fromDate for Year

--- src/Year.ts
+++ src/Year.ts
@@ -23,4 +23,22 @@ export class Year implements WithYear {
 
 		return new Year(year);
 	}
+
+	/**
+	 * Create a year from the given object.
+	 *
+	 * @param object
+	 */
+	public static from(object: WithYear) {
+		return this.of(object.year);
+	}
+
+	/**
+	 * Create a year from the given date.
+	 *
+	 * @param date
+	 */
+	public static fromDate(date: Date) {
+		return this.of(date.getFullYear());
+	}
 }
